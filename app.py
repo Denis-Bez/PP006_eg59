6fa0bfe91ffdf9a28943486a31a1895e3bc03cae
Denis-Bez <v417459@yandex.ru>
2022-08-15 00:06:11 +0500

    Create Structure

--- app.py
+++ app.py
@@ -23,11 +23,6 @@ app.register_blueprint(quality, url_prefix='/quality') # A Subsite of "Power qua
 
 # --- HEANDLERS BLOCK ---
 # Need to delete so many heandlers and do it dynamic generated. Remember DRY!
-@app.route('/test')
-def test():
-            return render_template('test.html')
-
-
 @app.route('/')
 def index():
         menu = get_menu()
