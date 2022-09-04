74742bc59ed7c3779d1f0dcda540afcbd4f5464b
Denis-Bez <v417459@yandex.ru>
2022-09-04 12:38:37 +0500

    desaign finish

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # Other libraries
 
 # Flask, WSGI libraries
-from flask import Flask, render_template
-from Class_SQLAlchemy import db, Menu, Solutions_menu, Product_menu
+from flask import Flask, render_template, request, redirect, flash
+from flask_mail import Mail, Message
+from Class_SQLAlchemy import Object, db, Menu, Solutions_menu, Product_menu
 
 # Configuratins and castom libraries
 from config import CONFIG
@@ -15,6 +16,16 @@ from Bushing.bushing import bushing
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///content.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["SECRET_KEY"] = CONFIG["SECRET_KEY"]
+app.config["MAIL_DEFAULT_SENDER"] = CONFIG["MAIL_DEFAULT_SENDER"]
+app.config["MAIL_PASSWORD"] = CONFIG["MAIL_PASSWORD"]
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_SERVER"] = "mail.eg-expert.ru"
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = CONFIG["MAIL_USERNAME"]
+mail = Mail(app)
 
 db.init_app(app)
 
@@ -58,9 +69,18 @@ def product():
 # Page with infotmation about the company's completed works
 @app.route('/objects')
 def objects():
-    content = get_all([Menu])
+    content = get_all([Menu, Object])
     if content:
-        return render_template('objects.html', title="Наши объекты", menu=content[0])
+        return render_template('objects.html', title="Наши объекты", menu=content[0], object_content=content[1])
+    else:
+        return render_template('error.html', title="Ошибка")
+
+
+@app.route('/objects/<int:object>')
+def object(object):
+    content = get_all([Menu, Object])
+    if content:
+        return render_template('layout_object.html', menu=content[0], object_content=content[1], object=object)
     else:
         return render_template('error.html', title="Ошибка")
 
@@ -75,13 +95,35 @@ def contact():
         return render_template('error.html', title="Ошибка")
 
 
-@app.route('/objects/<object>')
-def object(object):
-    menu = get_all([Menu])
-    if menu:
-        return render_template('layout_object.html', menu=menu, object=object)
-    else:
-        return render_template('error.html', title="Ошибка")
+@app.route("/email", methods=["POST", "GET"])
+def email():
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        phone = request.form.get("phone")
+        email = request.form.get("email")
+        text = request.form.get("text")
+
+        msg = Message("Заявка на экспертизу", recipients=["v417459@yandex.ru"])
+        msg_client = Message("Заявка успешно отправлена", recipients=[email])
+        msg_client.body = (f"Мы получили заявку на экспертизу. Свяжемся с вами в ближайшее время для уточнения информации")
+
+        # Sending mail
+        try:
+            mail.send(msg_client)
+            status = "Подтверждение на почту отправлено"
+        except:
+            status = "Подтверждение на почту не отправлено"
+
+        msg.body = (f"Имя клиента: {name}\nТелефон клиента: {phone}\nEmail заявки: {email}\nТекст заявки: {text}\nСтатус отправки письма клиенту: {status}")
+        
+        try:
+            mail.send(msg)
+            flash("Заявка успешно отправлена! Мы свяжемся с Вами в ближайшее время", category="success")
+        except:
+            flash("Произошла ошибка при отправке заявки. Попробуйте написать нам на почту expert@eg59.ru или позвонить по телефону +7 912-88-97-709", category="danger")
+        
+    return redirect ("/")
 
 
 @app.route('/test')
