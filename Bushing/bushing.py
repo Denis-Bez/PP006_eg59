3fef76c59f952e7b0e432854f502c428a3050fa5
Denis-Bez <v417459@yandex.ru>
2022-09-16 10:25:27 +0500

    Fillig content

--- Bushing/bushing.py
+++ Bushing/bushing.py
@@ -16,8 +16,9 @@ def index():
 @bushing.route('/<product>')
 def productCard(product):
     content = get_content()
+    seo = get_seo(product)
     if content:
-        return render_template('layout_productCard.html', menu=content[0], product_menu=content[1], url_name=product)
+        return render_template('layout_productCard.html', seo=seo, menu=content[0], product_menu=content[1], url_name=product)
     else:
         return render_template('error.html')
 
@@ -25,10 +26,12 @@ def productCard(product):
 @bushing.route('<products>/<product>')
 def productCards(products, product):
     content = get_content()
+    seo = get_seo(product)
     if content:
-        return render_template('layout_productCard.html', menu=content[0], product_menu=content[1], single_product=True, url_name=product)
+        return render_template('layout_productCard.html', seo=seo, menu=content[0], product_menu=content[1], single_product=True, url_name=product)
     else:
         return render_template('error.html')
+
 
 # --- DATBASE CONTENT GETTING ---
 def get_content():
@@ -38,5 +41,13 @@ def get_content():
         query_3 = Solution.query.filter_by(visibility='visible', solutions_menu_id='2').first()
         query_4 = SEO.query.filter_by(solutions_menu_id='1').all()
         return [query_1, query_2, query_3, query_4]
+    except:
+        return False
+
+
+def get_seo(url_name):
+    try:
+        res = SEO.query.filter_by(url_name=url_name).first()
+        return res 
     except:
         return False
