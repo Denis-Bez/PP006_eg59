6f37a82fd991afc00de196c588b17095d52994ba
Denis Bezvershenko <v417459@yandex.ru>
2023-03-15 11:28:15 +0500

    Expand spam list

--- spam_list.py
+++ spam_list.py
@@ -2,10 +2,11 @@
 # Проверку спам фильтра можно провести с помощью метода пересечения множеств (нужна сортировка)
 spam_filter = {
     'name': ['Zimer', 'RobertLot', 'https', 'http', 'RobertRemia', 'Clezerip', 'AlexBess', 'Alisa', 'Lot', 'David', 'Remia', 'William', 'Miprog',
-            'Albert', 'Fot', 'tetcher', 'Normanzer', 'Raymondtuh', 'Shisfieks', 'Ruby', 'Taylor', 'kzkazkayap', 'Oliverwxb', 'Olivergwg'
+            'Albert', 'Fot', 'tetcher', 'Normanzer', 'Raymondtuh', 'Shisfieks', 'Ruby', 'Taylor', 'kzkazkayap', 'Oliverwxb', 'Olivergwg', 'Kazkzrdvap',
+            'Alexandratib', 
             ],
     'phone': [],
-    'email': ['jenay454'],
+    'email': ['jenay454', 'kautksuhsk'],
     'text': ['ОЗОН.ру', 'Wildberries.ру', 'nуmрhоmaniа', 'нимфoманией', 'биceкcyальнa', 'любoвью', 'наркомании', 
     'Представьте, что Ваше коммерческое предложение', 'аутистам, онкобольным', '<a href=', 'porn',
     '\[url=', 'https://', 'http://', 'Http:', 'порно', '/кролика', 'Букмекеры', '.buzz', 'SEO продвижение', 'базы директоров', 
