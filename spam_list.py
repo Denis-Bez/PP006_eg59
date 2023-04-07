1fde36931870d6d75a12eacbacbc8e5ba411ed4d
Denis Bezvershenko <v417459@yandex.ru>
2023-04-07 12:58:41 +0500

    Expand spam list

--- spam_list.py
+++ spam_list.py
@@ -3,7 +3,7 @@
 spam_filter = {
     'name': ['Zimer', 'RobertLot', 'https', 'http', 'RobertRemia', 'Clezerip', 'AlexBess', 'Alisa', 'Lot', 'David', 'Remia', 'William', 'Miprog',
             'Albert', 'Fot', 'tetcher', 'Normanzer', 'Raymondtuh', 'Shisfieks', 'Ruby', 'Taylor', 'kzkazkayap', 'Oliverwxb', 'Olivergwg', 'Kazkzrdvap',
-            'Alexandratib', 'Sergio', 'Alexandratib', 'MikhailRV', 'KzkazSok', 'Kkkzellliz', 'Alexandratib', 'Kzkktofloum', 'casino'
+            'Alexandratib', 'Sergio', 'Alexandratib', 'MikhailRV', 'KzkazSok', 'Kkkzellliz', 'Alexandratib', 'Kzkktofloum', 'casino', 'Rolando'
             ],
     'phone': [],
     'email': ['jenay454', 'kautksuhsk'],
@@ -13,6 +13,6 @@ spam_filter = {
     'вирусную', 'Безлимитный интернет', 'Подберем новых', 'базы Директоров', 'Domain Rating', 'база Лидов', 'продвижением сайтов',
     'лидогенерация', 'SEO', 'прогон сайта', 'база Дизайнеров', 'повышу конверсию', 'копирайтинга', 'Хрумер', 'Заказать прогон', 'Кликфрод',
     'Яндекс Директа', 'Get millions', 'привлекаем клиентов', 'опубликовать книгу', 'www', 'секс', 'Gillian', 'Rininger', 'Срочно набираем сотрудников',
-    'Помогу вам увеличить', 'купить пару-тройку', 'very interesting', 'casino'
+    'Помогу вам увеличить', 'купить пару-тройку', 'very interesting', 'casino', 'farmacia'
     ]
 }
